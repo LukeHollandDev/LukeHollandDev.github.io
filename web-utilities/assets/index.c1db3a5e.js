a42de44104660c6ac12abf0a85d695a00310193e
AmazonPriime <lukeholland15500@gmail.com>
2022-05-05 18:51:37 +0000

    Deploying to main from @ AmazonPriime/Web-Utilities@fd5663c14954590fef715289ef06fe0da56d86ed 🚀

--- web-utilities/assets/index.c1db3a5e.js
+++ web-utilities/assets/index.c1db3a5e.js
@@ -8817,22 +8817,67 @@ var _export_sfc = (sfc, props) => {
   }
   return target;
 };
-const _sfc_main$8 = {};
-const _hoisted_1$7 = {
+const _sfc_main$b = {};
+const _hoisted_1$a = {
   xmlns: "http://www.w3.org/2000/svg",
   width: "25",
   height: "25",
   fill: "currentColor",
   viewBox: "0 0 512 512"
 };
-const _hoisted_2$7 = /* @__PURE__ */ createBaseVNode("path", { d: "M0 96C0 78.33 14.33 64 32 64H416C433.7 64 448 78.33 448 96C448 113.7 433.7 128 416 128H32C14.33 128 0 113.7 0 96zM0 256C0 238.3 14.33 224 32 224H416C433.7 224 448 238.3 448 256C448 273.7 433.7 288 416 288H32C14.33 288 0 273.7 0 256zM416 448H32C14.33 448 0 433.7 0 416C0 398.3 14.33 384 32 384H416C433.7 384 448 398.3 448 416C448 433.7 433.7 448 416 448z" }, null, -1);
+const _hoisted_2$9 = /* @__PURE__ */ createBaseVNode("path", { d: "M0 96C0 78.33 14.33 64 32 64H416C433.7 64 448 78.33 448 96C448 113.7 433.7 128 416 128H32C14.33 128 0 113.7 0 96zM0 256C0 238.3 14.33 224 32 224H416C433.7 224 448 238.3 448 256C448 273.7 433.7 288 416 288H32C14.33 288 0 273.7 0 256zM416 448H32C14.33 448 0 433.7 0 416C0 398.3 14.33 384 32 384H416C433.7 384 448 398.3 448 416C448 433.7 433.7 448 416 448z" }, null, -1);
+const _hoisted_3$9 = [
+  _hoisted_2$9
+];
+function _sfc_render$4(_ctx, _cache) {
+  return openBlock(), createElementBlock("svg", _hoisted_1$a, _hoisted_3$9);
+}
+var BarsIcon = /* @__PURE__ */ _export_sfc(_sfc_main$b, [["render", _sfc_render$4]]);
+const _sfc_main$a = {};
+const _hoisted_1$9 = {
+  xmlns: "http://www.w3.org/2000/svg",
+  height: "40px",
+  fill: "currentColor",
+  viewBox: "0 0 512 512"
+};
+const _hoisted_2$8 = /* @__PURE__ */ createBaseVNode("path", { d: "M320 464C328.8 464 336 456.8 336 448V416H384V448C384 483.3 355.3 512 320 512H64C28.65 512 0 483.3 0 448V416H48V448C48 456.8 55.16 464 64 464H320zM256 160C238.3 160 224 145.7 224 128V48H64C55.16 48 48 55.16 48 64V192H0V64C0 28.65 28.65 0 64 0H229.5C246.5 0 262.7 6.743 274.7 18.75L365.3 109.3C377.3 121.3 384 137.5 384 154.5V192H336V160H256zM88 224C118.9 224 144 249.1 144 280C144 310.9 118.9 336 88 336H80V368C80 376.8 72.84 384 64 384C55.16 384 48 376.8 48 368V240C48 231.2 55.16 224 64 224H88zM112 280C112 266.7 101.3 256 88 256H80V304H88C101.3 304 112 293.3 112 280zM160 240C160 231.2 167.2 224 176 224H200C226.5 224 248 245.5 248 272V336C248 362.5 226.5 384 200 384H176C167.2 384 160 376.8 160 368V240zM192 352H200C208.8 352 216 344.8 216 336V272C216 263.2 208.8 256 200 256H192V352zM336 224C344.8 224 352 231.2 352 240C352 248.8 344.8 256 336 256H304V288H336C344.8 288 352 295.2 352 304C352 312.8 344.8 320 336 320H304V368C304 376.8 296.8 384 288 384C279.2 384 272 376.8 272 368V240C272 231.2 279.2 224 288 224H336z" }, null, -1);
+const _hoisted_3$8 = [
+  _hoisted_2$8
+];
+function _sfc_render$3(_ctx, _cache) {
+  return openBlock(), createElementBlock("svg", _hoisted_1$9, _hoisted_3$8);
+}
+var PDFIcon = /* @__PURE__ */ _export_sfc(_sfc_main$a, [["render", _sfc_render$3]]);
+const _sfc_main$9 = {};
+const _hoisted_1$8 = {
+  xmlns: "http://www.w3.org/2000/svg",
+  height: "40px",
+  fill: "currentColor",
+  viewBox: "0 0 512 512"
+};
+const _hoisted_2$7 = /* @__PURE__ */ createBaseVNode("path", { d: "M448 352V48C448 21.53 426.5 0 400 0h-320C35.89 0 0 35.88 0 80v352C0 476.1 35.89 512 80 512h344c13.25 0 24-10.75 24-24s-10.75-24-24-24H416v-66.95C434.6 390.4 448 372.8 448 352zM368 464h-288c-17.64 0-32-14.34-32-32s14.36-32 32-32h288V464zM400 352h-320c-11.38 0-22.2 2.375-32 6.688V80c0-17.66 14.36-32 32-32h320V352zM152 160h176C341.3 160 352 149.3 352 136S341.3 112 328 112h-176C138.8 112 128 122.8 128 136S138.8 160 152 160zM152 240h176C341.3 240 352 229.3 352 216S341.3 192 328 192h-176C138.8 192 128 202.8 128 216S138.8 240 152 240z" }, null, -1);
 const _hoisted_3$7 = [
   _hoisted_2$7
 ];
 function _sfc_render$2(_ctx, _cache) {
-  return openBlock(), createElementBlock("svg", _hoisted_1$7, _hoisted_3$7);
+  return openBlock(), createElementBlock("svg", _hoisted_1$8, _hoisted_3$7);
 }
-var BarsIcon = /* @__PURE__ */ _export_sfc(_sfc_main$8, [["render", _sfc_render$2]]);
+var BookIcon = /* @__PURE__ */ _export_sfc(_sfc_main$9, [["render", _sfc_render$2]]);
+const _sfc_main$8 = {};
+const _hoisted_1$7 = {
+  xmlns: "http://www.w3.org/2000/svg",
+  height: "40px",
+  fill: "currentColor",
+  viewBox: "0 0 512 512"
+};
+const _hoisted_2$6 = /* @__PURE__ */ createBaseVNode("path", { d: "M505.2 324.8l-47.73-68.78l47.75-68.81c7.359-10.62 8.797-24.12 3.844-36.06c-4.969-11.94-15.52-20.44-28.22-22.72l-82.39-14.88l-14.89-82.41c-2.281-12.72-10.76-23.25-22.69-28.22c-11.97-4.936-25.42-3.498-36.12 3.844L256 54.49L187.2 6.709C176.5-.6016 163.1-2.039 151.1 2.896c-11.92 4.971-20.4 15.5-22.7 28.19l-14.89 82.44L31.15 128.4C18.42 130.7 7.854 139.2 2.9 151.2C-2.051 163.1-.5996 176.6 6.775 187.2l47.73 68.78l-47.75 68.81c-7.359 10.62-8.795 24.12-3.844 36.06c4.969 11.94 15.52 20.44 28.22 22.72l82.39 14.88l14.89 82.41c2.297 12.72 10.78 23.25 22.7 28.22c11.95 4.906 25.44 3.531 36.09-3.844L256 457.5l68.83 47.78C331.3 509.7 338.8 512 346.3 512c4.906 0 9.859-.9687 14.56-2.906c11.92-4.969 20.4-15.5 22.7-28.19l14.89-82.44l82.37-14.88c12.73-2.281 23.3-10.78 28.25-22.75C514.1 348.9 512.6 335.4 505.2 324.8zM456.8 339.2l-99.61 18l-18 99.63L256 399.1L172.8 456.8l-18-99.63l-99.61-18L112.9 255.1L55.23 172.8l99.61-18l18-99.63L256 112.9l83.15-57.75l18.02 99.66l99.61 18L399.1 255.1L456.8 339.2zM256 143.1c-61.85 0-111.1 50.14-111.1 111.1c0 61.85 50.15 111.1 111.1 111.1s111.1-50.14 111.1-111.1C367.1 194.1 317.8 143.1 256 143.1zM256 319.1c-35.28 0-63.99-28.71-63.99-63.99S220.7 192 256 192s63.99 28.71 63.99 63.1S291.3 319.1 256 319.1z" }, null, -1);
+const _hoisted_3$6 = [
+  _hoisted_2$6
+];
+function _sfc_render$1(_ctx, _cache) {
+  return openBlock(), createElementBlock("svg", _hoisted_1$7, _hoisted_3$6);
+}
+var SunIcon = /* @__PURE__ */ _export_sfc(_sfc_main$8, [["render", _sfc_render$1]]);
 const _sfc_main$7 = {};
 const _hoisted_1$6 = {
   xmlns: "http://www.w3.org/2000/svg",
@@ -8840,31 +8885,47 @@ const _hoisted_1$6 = {
   fill: "currentColor",
   viewBox: "0 0 512 512"
 };
-const _hoisted_2$6 = /* @__PURE__ */ createBaseVNode("path", { d: "M320 464C328.8 464 336 456.8 336 448V416H384V448C384 483.3 355.3 512 320 512H64C28.65 512 0 483.3 0 448V416H48V448C48 456.8 55.16 464 64 464H320zM256 160C238.3 160 224 145.7 224 128V48H64C55.16 48 48 55.16 48 64V192H0V64C0 28.65 28.65 0 64 0H229.5C246.5 0 262.7 6.743 274.7 18.75L365.3 109.3C377.3 121.3 384 137.5 384 154.5V192H336V160H256zM88 224C118.9 224 144 249.1 144 280C144 310.9 118.9 336 88 336H80V368C80 376.8 72.84 384 64 384C55.16 384 48 376.8 48 368V240C48 231.2 55.16 224 64 224H88zM112 280C112 266.7 101.3 256 88 256H80V304H88C101.3 304 112 293.3 112 280zM160 240C160 231.2 167.2 224 176 224H200C226.5 224 248 245.5 248 272V336C248 362.5 226.5 384 200 384H176C167.2 384 160 376.8 160 368V240zM192 352H200C208.8 352 216 344.8 216 336V272C216 263.2 208.8 256 200 256H192V352zM336 224C344.8 224 352 231.2 352 240C352 248.8 344.8 256 336 256H304V288H336C344.8 288 352 295.2 352 304C352 312.8 344.8 320 336 320H304V368C304 376.8 296.8 384 288 384C279.2 384 272 376.8 272 368V240C272 231.2 279.2 224 288 224H336z" }, null, -1);
-const _hoisted_3$6 = [
-  _hoisted_2$6
-];
-function _sfc_render$1(_ctx, _cache) {
-  return openBlock(), createElementBlock("svg", _hoisted_1$6, _hoisted_3$6);
-}
-var PDFIcon = /* @__PURE__ */ _export_sfc(_sfc_main$7, [["render", _sfc_render$1]]);
-const _sfc_main$6 = {};
-const _hoisted_1$5 = {
-  xmlns: "http://www.w3.org/2000/svg",
-  height: "40px",
-  fill: "currentColor",
-  viewBox: "0 0 512 512"
-};
-const _hoisted_2$5 = /* @__PURE__ */ createBaseVNode("path", { d: "M448 352V48C448 21.53 426.5 0 400 0h-320C35.89 0 0 35.88 0 80v352C0 476.1 35.89 512 80 512h344c13.25 0 24-10.75 24-24s-10.75-24-24-24H416v-66.95C434.6 390.4 448 372.8 448 352zM368 464h-288c-17.64 0-32-14.34-32-32s14.36-32 32-32h288V464zM400 352h-320c-11.38 0-22.2 2.375-32 6.688V80c0-17.66 14.36-32 32-32h320V352zM152 160h176C341.3 160 352 149.3 352 136S341.3 112 328 112h-176C138.8 112 128 122.8 128 136S138.8 160 152 160zM152 240h176C341.3 240 352 229.3 352 216S341.3 192 328 192h-176C138.8 192 128 202.8 128 216S138.8 240 152 240z" }, null, -1);
+const _hoisted_2$5 = /* @__PURE__ */ createBaseVNode("path", { d: "M421.6 379.9c-.6641 0-1.35 .0625-2.049 .1953c-11.24 2.143-22.37 3.17-33.32 3.17c-94.81 0-174.1-77.14-174.1-175.5c0-63.19 33.79-121.3 88.73-152.6c8.467-4.812 6.339-17.66-3.279-19.44c-11.2-2.078-29.53-3.746-40.9-3.746C132.3 31.1 32 132.2 32 256c0 123.6 100.1 224 223.8 224c69.04 0 132.1-31.45 173.8-82.93C435.3 389.1 429.1 379.9 421.6 379.9zM255.8 432C158.9 432 80 353 80 256c0-76.32 48.77-141.4 116.7-165.8C175.2 125 163.2 165.6 163.2 207.8c0 99.44 65.13 183.9 154.9 212.8C298.5 428.1 277.4 432 255.8 432z" }, null, -1);
 const _hoisted_3$5 = [
   _hoisted_2$5
 ];
 function _sfc_render(_ctx, _cache) {
-  return openBlock(), createElementBlock("svg", _hoisted_1$5, _hoisted_3$5);
+  return openBlock(), createElementBlock("svg", _hoisted_1$6, _hoisted_3$5);
 }
-var BookIcon = /* @__PURE__ */ _export_sfc(_sfc_main$6, [["render", _sfc_render]]);
+var MoonIcon = /* @__PURE__ */ _export_sfc(_sfc_main$7, [["render", _sfc_render]]);
+var ThemeSwitcher_vue_vue_type_style_index_0_scoped_true_lang = "";
+const _withScopeId$5 = (n2) => (pushScopeId("data-v-13817600"), n2 = n2(), popScopeId(), n2);
+const _hoisted_1$5 = /* @__PURE__ */ _withScopeId$5(() => /* @__PURE__ */ createBaseVNode("br", null, null, -1));
+const _sfc_main$6 = {
+  setup(__props) {
+    const theme = ref(localStorage.getItem("theme") || "light");
+    function switchTheme() {
+      if (localStorage.getItem("theme") === "light") {
+        localStorage.setItem("theme", "dark");
+        theme.value = "dark";
+        document.documentElement.setAttribute("data-theme", "dark");
+      } else {
+        localStorage.setItem("theme", "light");
+        theme.value = "light";
+        document.documentElement.setAttribute("data-theme", "light");
+      }
+    }
+    return (_ctx, _cache) => {
+      return openBlock(), createElementBlock("div", {
+        class: "theme-container",
+        onClick: switchTheme
+      }, [
+        theme.value === "dark" ? (openBlock(), createBlock(SunIcon, { key: 0 })) : createCommentVNode("", true),
+        theme.value === "light" ? (openBlock(), createBlock(MoonIcon, { key: 1 })) : createCommentVNode("", true),
+        _hoisted_1$5,
+        createTextVNode(" " + toDisplayString(theme.value) + " mode ", 1)
+      ]);
+    };
+  }
+};
+var ThemeSwitcher = /* @__PURE__ */ _export_sfc(_sfc_main$6, [["__scopeId", "data-v-13817600"]]);
 var NavBar_vue_vue_type_style_index_0_scoped_true_lang = "";
-const _withScopeId$4 = (n2) => (pushScopeId("data-v-9bbc360a"), n2 = n2(), popScopeId(), n2);
+const _withScopeId$4 = (n2) => (pushScopeId("data-v-087f39de"), n2 = n2(), popScopeId(), n2);
 const _hoisted_1$4 = { class: "title" };
 const _hoisted_2$4 = /* @__PURE__ */ _withScopeId$4(() => /* @__PURE__ */ createBaseVNode("h2", null, "Tools", -1));
 const _hoisted_3$4 = { class: "options-container" };
@@ -8914,12 +8975,13 @@ const _sfc_main$5 = {
             ]),
             _: 1
           })
-        ])
+        ]),
+        createVNode(ThemeSwitcher)
       ], 2);
     };
   }
 };
-var NavBar = /* @__PURE__ */ _export_sfc(_sfc_main$5, [["__scopeId", "data-v-9bbc360a"]]);
+var NavBar = /* @__PURE__ */ _export_sfc(_sfc_main$5, [["__scopeId", "data-v-087f39de"]]);
 var TopBar_vue_vue_type_style_index_0_scoped_true_lang = "";
 const _withScopeId$3 = (n2) => (pushScopeId("data-v-6457d768"), n2 = n2(), popScopeId(), n2);
 const _hoisted_1$3 = { class: "container" };
